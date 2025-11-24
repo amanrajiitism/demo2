c3ad0b700cf1bc14ccce6ed00ce70e5402ca94de
Aman Raj <amanrajiitism@gmail.com>
2025-11-24 17:04:49 +0530

    addition logic

--- src/main/java/org/example/HelloController.java
+++ src/main/java/org/example/HelloController.java
@@ -1,7 +1,12 @@
 package org.example;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @RestController
 public class HelloController {
@@ -9,5 +14,16 @@ public class HelloController {
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello Aman! Your microservice is working.";
+    }
+
+    @GetMapping("/sum")
+    public Map<String, Object> getSum(@RequestParam int a, @RequestParam int b) {
+        Map<String, Object> response = new LinkedHashMap<>();
+        int sum = a+b;
+        response.put("message :" , "calculation successful");
+        response.put("first number to add :" , a);
+        response.put("second number to add : ", b);
+        response.put("summation of the two numbers is : ", sum);
+        return response;
     }
 }
