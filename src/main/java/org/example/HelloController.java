63d6c49b692fd23829e64692e038f9c6f46ba88a
Aman Raj <amanrajiitism@gmail.com>
2025-11-26 16:02:18 +0530

    add config files

--- src/main/java/org/example/HelloController.java
+++ src/main/java/org/example/HelloController.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+
 @RestController
 public class HelloController {
+
+    @Value("${calculation.type}")
+    private String calculationType;
 
     private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
     @GetMapping("/hello")
@@ -23,6 +28,7 @@ public class HelloController {
     public Map<String, Object> getSum(@RequestParam int a, @RequestParam int b) {
         logger.info("Received request to calculate sum: a={}, b={}", a, b);
         Map<String, Object> response = new LinkedHashMap<>();
+
         int sum = a+b;
         response.put("message :" , "calculation successful");
         response.put("first number to add :" , a);
@@ -31,4 +37,31 @@ public class HelloController {
         logger.debug("Calculation completed. Result={}", sum);
         return response;
     }
+
+    @GetMapping("/divide")
+    public Map<String, Object> division(@RequestParam int a, @RequestParam int b) {
+        logger.info("Received request to divide: a={}, b={}", a, b);
+        Map<String, Object> response = new LinkedHashMap<>();
+        if (b == 0) {
+
+            response.put("message :", "invalid input");
+            return response;
+        }
+        if (calculationType.equalsIgnoreCase("int")) {
+             int result = a / b;
+            response.put("division of the two integers is : ", result);
+            logger.debug("Calculation for division completed Result={}", result);
+        } else {
+           float result = (int) ((float) a / b);
+            response.put("division of the two numbers is : ", result);
+            logger.debug("Calculation completed Result={}", result);
+        }
+        response.put("message :", "calculation successful");
+        response.put("first number to divide :", a);
+        response.put("second number to divide with : ", b);
+
+        return response;
+        }
+
+
 }
